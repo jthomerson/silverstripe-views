7b03e9a74a288df175f99f6a5fe464faa08b3a7b
Jeremy Thomerson <jeremy@thomersonfamily.com>
2012-01-30 18:35:06 +0000

    Initial commit - _config.php file

---
+++ _config.php
@@ -0,0 +1,12 @@
+<?php
+
+/* The silverstripe-views module provides a way for UI (theme) designers and
+ * content organizers to define dynamic "views", or queries into the content
+ * system, to utilize in their templates.  These views can then power any
+ * number of interface features and widgets without a developer needing to
+ * write custom query functions that can be called from the SilverStripe
+ * templates (in control tags).
+ *
+ * This config file currently contains no configuration, but must exist for
+ * SilverStripe to recognize this folder as a module.
+ */
